27dd0cb24830aa8bacdfb7ef1230d2726d9ad53a
フリド <saidi-farid@altplus.co.jp>
2016-03-17 18:39:22 +0900

    Memory Leak Without Animation

---
+++ ViewControllerMemoryLeak/SecondViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SecondViewController.swift
+//  ViewControllerMemoryLeak
+//
+//  Created by Saidi Farid on 3/17/16.
+//  Copyright © 2016 Fafrotskies. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("Second ViewController Loaded Successfully!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func dismiss(sender: UIButton)
+    {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    deinit
+    {
+        print("Second ViewController De-Inited Successfully!")
+    }
+}
